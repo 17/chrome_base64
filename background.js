1ea6906efd6df9b724f7083acbdbc06af48579ec
Jase Su <unmric@gmail.com>
2019-01-10 01:01:21 +0800

    v0.2.0

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
 chrome.contextMenus.create({
   id: 'chrome_base64',
   // title: 'Base64 encode/decode selected text"',
-  title: 'Base64 encode/decode "%s"',
-  // title: 'Base64 加解密 "%s"',
+  // title: 'Base64 encode/decode "%s"',
+  title: '使用 Base64 加解密 "%s"',
   contexts: ['selection', 'editable'],
   onclick: function (info, tab) {
-    console.log(info, tab)
-    // const type = info.editable ?
+    // console.log(info, tab)
+    // info.editable ?
     chrome.tabs.sendMessage(tab.id, {
       action: 'click'
     })
--- content_scripts.js
+++ content_scripts.js
@@ -19,68 +19,76 @@ String.prototype.replaceBetween = function(start, end, what) {
 
 function decode_or_encode_selection() {
   const sel = window.getSelection()
-  console.log(sel)
+  // console.log(sel)
+
+  // 是否在同一个节点 (是否可以被替换)
   if (sel.anchorNode == sel.focusNode) {
-    // 是否在同一个节点
+    // 表单元素
+    // 表单元素最大特点 isCollapsed === ture
     if (sel.isCollapsed) {
-      let el
-      // 表单元素最大特点 isCollapsed === ture
       try {
         // 以防意外
-        el = sel.anchorNode.childNodes[sel.extentOffset]
-      } catch (e) {
-        alert('获取元素错误')
+        var el = sel.anchorNode.childNodes[sel.extentOffset]
+      } catch (error) {
+        alert('获取元素错误，请重试。')
         return
       }
-      // console.log(el.nodeName, ~['TEXTAREA', 'INPUT'].indexOf(el.nodeName))
       if (~['TEXTAREA', 'INPUT'].indexOf(el.nodeName)) {
         const start = el.selectionStart
         const end = el.selectionEnd
         const sel_str = el.value.substring(start, end)
-        const r_str = is_base64(sel_str)? decodeURIComponent(escape(window.atob(sel_str))): window.btoa(unescape(encodeURIComponent(sel_str)))
+        const r_str = Base64.decode_or_encode(sel_str)
         el.value = el.value.replaceBetween(start, end, r_str)
         return
       }
-      alert('暂不支持此元素')
-    } else {
-      if (sel.anchorNode.nodeName == '#text') {
-        let start = sel.anchorOffset
-        let end = sel.extentOffset
-        if (end < start) {
-          start = sel.extentOffset
-          end = sel.anchorOffset
-        }
-        const sel_str = sel.anchorNode.textContent.substring(start, end)
-        const r_str = is_base64(sel_str)? decodeURIComponent(escape(window.atob(sel_str))): window.btoa(unescape(encodeURIComponent(sel_str)))
-        sel.anchorNode.textContent = sel.anchorNode.textContent.replaceBetween(start, end, r_str)
-        return
+    } 
+
+    // 文本元素
+    if (sel.anchorNode.nodeName == '#text') {
+      let start = sel.anchorOffset
+      let end = sel.extentOffset
+      if (end < start) {
+        start = sel.extentOffset
+        end = sel.anchorOffset
       }
-      alert('暂不支持此元素')
+      const sel_str = sel.anchorNode.textContent.substring(start, end)
+      const r_str = Base64.decode_or_encode(sel_str)
+      sel.anchorNode.textContent = sel.anchorNode.textContent.replaceBetween(start, end, r_str)
+      return
     }
-  } else {
-    alert('暂不支持跨多个元素转换')
   }
+
+  // 默认
+  const sel_str = sel.toString()
+  const r_str = Base64.decode_or_encode(sel_str)
+  // const tips = (Base64.is_base64(sel_str)? '解密': '加密') + '完成'
+  prompt('', r_str)
 }
 
-
-function is_base64(str) {
-  // https://github.com/miguelmota/is-base64/blob/master/is-base64.js#L15
-  var regex = '(?:[A-Za-z0-9+\\/]{4})*(?:[A-Za-z0-9+\\/]{2}==|[A-Za-z0-9+\/]{3}=)?'
-  // return (new RegExp('^' + regex + '$', 'gi')).test(str)
-  if (!(new RegExp('^' + regex + '$', 'gi')).test(str)) {
-    return false
+const Base64 = {
+  decode(str) {
+    return decodeURIComponent(escape(window.atob(str)))
+  },
+  encode(str) {
+    return window.btoa(unescape(encodeURIComponent(str)))
+  },
+  is_base64(str) {
+    // https://github.com/miguelmota/is-base64/blob/master/is-base64.js#L15
+    // var regex = '(?:[A-Za-z0-9+\\/]{4})*(?:[A-Za-z0-9+\\/]{2}==|[A-Za-z0-9+\/]{3}=)?';
+    // return (new RegExp('^' + regex + '$', 'gi')).test(str)
+    const regex = /^(?:[A-Za-z0-9+\\/]{4})*(?:[A-Za-z0-9+\\/]{2}==|[A-Za-z0-9+\/]{3}=)?$/gi
+    if (!regex.test(str)) {
+      return false
+    }
+    try {
+      this.decode(str)
+    } catch (error) {
+      // Malformed UTF-8 data
+      return false
+    }
+    return true
+  },
+  decode_or_encode(str) {
+    return this.is_base64(str)? this.decode(str): this.encode(str)
   }
-  try {
-    decodeURIComponent(escape(window.atob(str)))
-  } catch (error) {
-    // Malformed UTF-8 data
-    return false
-  }
-  return true
-  // try {
-  //   decode_str = window.atob(str)
-  // } catch(e) {
-  //   return [false, '']
-  // }
-  // return [true, decode_str]
 }
